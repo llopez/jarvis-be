1cb1d3083887c99cbd284702eab3d1cd40e0f3dd
Luis Lopez <luigibyte@gmail.com>
2018-05-16 19:45:14 -0300

    Disable ActionCable Request forgery protection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-
+  config.action_cable.disable_request_forgery_protection = true
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
